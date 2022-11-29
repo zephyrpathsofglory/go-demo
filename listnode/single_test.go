412d3dc4d3f839a734fe1a004168a8b295967488
harden <harden@shundaojia.com>
2022-11-29 18:35:50 +0800

    feat: single linked list
    Signed-off-by: harden <harden@shundaojia.com>

---
+++ listnode/single_test.go
@@ -0,0 +1,491 @@
+package listnode_test
+
+import (
+	"testing"
+
+	"github.com/go-demo/listnode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveElements(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.RemoveElements(head, 1)
+
+		nums := make([]int, 0, 4)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{2, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 1,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.RemoveElements(head, 1)
+
+		nums := make([]int, 0, 4)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{2, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.RemoveElements(head, 2)
+
+		nums := make([]int, 0, 4)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  2,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.RemoveElements(head, 2)
+
+		nums := make([]int, 0, 4)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 3})
+	}
+}
+
+func TestGet(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		assert.Equal(t, listnode.Get(head, 0), 1)
+		assert.Equal(t, listnode.Get(head, 1), 2)
+		assert.Equal(t, listnode.Get(head, 2), 3)
+		assert.Equal(t, listnode.Get(head, 3), 4)
+		assert.Equal(t, listnode.Get(head, 4), -1)
+		assert.Equal(t, listnode.Get(head, 5), -1)
+		assert.Equal(t, listnode.Get(head, 6), -1)
+	}
+
+	{
+		var head *listnode.ListNode
+
+		assert.Equal(t, listnode.Get(head, 0), -1)
+		assert.Equal(t, listnode.Get(head, 1), -1)
+		assert.Equal(t, listnode.Get(head, 2), -1)
+		assert.Equal(t, listnode.Get(head, 3), -1)
+		assert.Equal(t, listnode.Get(head, 4), -1)
+		assert.Equal(t, listnode.Get(head, 5), -1)
+		assert.Equal(t, listnode.Get(head, 6), -1)
+
+	}
+}
+
+func TestAddAtHead(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtHead(head, 5)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{5, 1, 2, 3, 4})
+	}
+
+	{
+		var head *listnode.ListNode
+		res := listnode.AddAtHead(head, 1)
+
+		nums := make([]int, 0, 1)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1})
+	}
+}
+
+func TestAddAtTail(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtTail(head, 5)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 4, 5})
+	}
+
+	{
+		var head *listnode.ListNode
+
+		res := listnode.AddAtTail(head, 1)
+
+		nums := make([]int, 0, 1)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1})
+	}
+}
+
+func TestAddAtIndex(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtIndex(head, 5, 1000)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtIndex(head, 4, 1000)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 4, 1000})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtIndex(head, 3, 1000)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 1000, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtIndex(head, -1, 1000)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1000, 1, 2, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.AddAtIndex(head, 0, 1000)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1000, 1, 2, 3, 4})
+	}
+}
+
+func TestDeleteAtIndex(t *testing.T) {
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.DeleteAtIndex(head, 3)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.DeleteAtIndex(head, -1)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.DeleteAtIndex(head, 0)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{2, 3, 4})
+	}
+
+	{
+		head := &listnode.ListNode{
+			Val: 1,
+			Next: &listnode.ListNode{
+				Val: 2,
+				Next: &listnode.ListNode{
+					Val: 3,
+					Next: &listnode.ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}
+
+		res := listnode.DeleteAtIndex(head, 10)
+
+		nums := make([]int, 0, 5)
+		cur := res
+		for cur != nil {
+			nums = append(nums, cur.Val)
+			cur = cur.Next
+		}
+
+		assert.Equal(t, nums, []int{1, 2, 3, 4})
+	}
+}
