e1c79b1fdfb01d0c69a0ff745db63afebd5f1c71
harden <harden@shundaojia.com>
2023-01-13 19:57:14 +0800

    node methods
    Signed-off-by: harden <harden@shundaojia.com>

--- tree/node.go
+++ tree/node.go
@@ -60,3 +60,67 @@ func inorderTraversal(root *TreeNode) (res []int) {
 
 	return
 }
+
+func levelTraversal(root *TreeNode) (res [][]int) {
+	var traversal func(*TreeNode, int)
+	traversal = func(root *TreeNode, depth int) {
+		if root == nil {
+			return
+		}
+
+		if len(res) == depth {
+			res = append(res, []int{})
+		}
+
+		res[depth] = append(res[depth], root.Val)
+
+		traversal(root.Left, depth+1)
+		traversal(root.Right, depth+1)
+	}
+
+	traversal(root, 0)
+
+	return
+}
+
+// 二叉树的右视图
+func rightSideView(root *TreeNode) (res []int) {
+	var traversal func(*TreeNode, int)
+	traversal = func(root *TreeNode, depth int) {
+		if root == nil {
+			return
+		}
+
+		if len(res) == depth {
+			res = append(res, root.Val)
+		} else {
+			res[depth] = root.Val
+		}
+
+		traversal(root.Left, depth+1)
+		traversal(root.Right, depth+1)
+	}
+
+	traversal(root, 0)
+
+	return
+}
+
+func invert(root *TreeNode) {
+	var invert func(*TreeNode)
+
+	invert = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		root.Left, root.Right = root.Right, root.Left
+
+		invert(root.Right)
+		invert(root.Left)
+	}
+
+	invert(root)
+
+	return
+}
--- tree/node_test.go
+++ tree/node_test.go
@@ -86,3 +86,94 @@ func TestInorderTraversal(t *testing.T) {
 	res := inorderTraversal(tree)
 	assert.Equal(t, []int{3, 6, 8, 10, 12, 15, 18}, res)
 }
+
+func TestLevelTraversal(t *testing.T) {
+	tree := &TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 6,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 8,
+			},
+		},
+		Right: &TreeNode{
+			Val: 15,
+			Left: &TreeNode{
+				Val: 12,
+			},
+			Right: &TreeNode{
+				Val: 18,
+			},
+		},
+	}
+
+	res := levelTraversal(tree)
+	assert.Equal(t, [][]int{
+		{10},
+		{6, 15},
+		{3, 8, 12, 18},
+	}, res)
+}
+
+func TestRightSideView(t *testing.T) {
+	tree := &TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 6,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 8,
+			},
+		},
+		Right: &TreeNode{
+			Val: 15,
+			Left: &TreeNode{
+				Val: 12,
+			},
+			Right: &TreeNode{
+				Val: 18,
+			},
+		},
+	}
+
+	res := rightSideView(tree)
+	assert.Equal(t, []int{10, 15, 18}, res)
+}
+
+func TestInvert(t *testing.T) {
+	tree := &TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 6,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 8,
+			},
+		},
+		Right: &TreeNode{
+			Val: 15,
+			Left: &TreeNode{
+				Val: 12,
+			},
+			Right: &TreeNode{
+				Val: 18,
+			},
+		},
+	}
+
+	invert(tree)
+	assert.Equal(t, 10, tree.Val)
+	assert.Equal(t, 15, tree.Left.Val)
+	assert.Equal(t, 18, tree.Left.Left.Val)
+	assert.Equal(t, 12, tree.Left.Right.Val)
+	assert.Equal(t, 6, tree.Right.Val)
+	assert.Equal(t, 8, tree.Right.Left.Val)
+	assert.Equal(t, 3, tree.Right.Right.Val)
+}
