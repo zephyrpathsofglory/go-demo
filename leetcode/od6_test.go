c3348ebda70ecaed8b6e16e0670676a0d26b20bf
harden <harden@shundaojia.com>
2024-03-20 18:55:21 +0800

    add od exam test

--- leetcode/od6_test.go
+++ leetcode/od6_test.go
@@ -7,13 +7,28 @@ import (
 )
 
 func TestMazeWalker(t *testing.T) {
-	tc, uc := mazeWalker(6, 4, [][2]int{
-		{0, 2},
-		{1, 2},
-		{2, 2},
-		{4, 1},
-		{5, 1},
-	})
-	assert.Equal(t, 2, tc)
-	assert.Equal(t, 3, uc)
+	{
+		tc, uc := mazeWalker(6, 4, [][2]int{
+			{0, 2},
+			{1, 2},
+			{2, 2},
+			{4, 1},
+			{5, 1},
+		})
+		assert.Equal(t, 2, tc)
+		assert.Equal(t, 3, uc)
+	}
+
+	{
+		{
+			tc, uc := mazeWalker(6, 4, [][2]int{
+				{2, 0},
+				{2, 1},
+				{3, 0},
+				{3, 1},
+			})
+			assert.Equal(t, 0, tc)
+			assert.Equal(t, 4, uc)
+		}
+	}
 }
