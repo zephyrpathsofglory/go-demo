045102696a1692a75f9fe981a139cd2e093b94a3
zephyrpathsofglory <zhangyonguu@gmail.com>
2024-03-30 17:17:07 +0800

    add test case

--- od/od12_test.go
+++ od/od12_test.go
@@ -11,4 +11,9 @@ func TestDecompression(t *testing.T) {
 	assert.Equal(t, "mccmccmcc", decompression("3[m2[c]]"))
 	assert.Equal(t, "hmccmccmcc", decompression("h3[m2[c]]"))
 	assert.Equal(t, "hmccmccmccg", decompression("h3[m2[c]]g"))
+	assert.Equal(t, "kkmnmn", decompression("2[k]2[mn]"))
+	assert.Equal(t, "mccmcc", decompression("2[m2[c]]"))
+	assert.Equal(t, "wc", decompression("1[w]c"))
+	assert.Equal(t, "wcwc", decompression("2[wc]"))
+	assert.Equal(t, "wkkkcwkkkc", decompression("2[w3[k]c]"))
 }
