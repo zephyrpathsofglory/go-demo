1bec01a3cb9074d4e8bb1b19182912696ca60d9e
zephyrpathsofglory <zhangyonguu@gmail.com>
2024-05-06 01:51:57 +0800

    add leetcode exam tests

--- leetcode/84_test.go
+++ leetcode/84_test.go
@@ -7,6 +7,20 @@ import (
 )
 
 func TestLargestRectangleArea(t *testing.T) {
-	heights := []int{1, 3, 4, 5, 2, 7, 4}
-	assert.Equal(t, 12, LargestRectangleArea(heights))
+	{
+		heights := []int{1, 3, 4, 5, 2, 7, 4}
+		assert.Equal(t, 12, LargestRectangleArea(heights))
+	}
+	{
+		heights := []int{1, 3, 4, 500, 2, 7, 4}
+		assert.Equal(t, 500, LargestRectangleArea(heights))
+	}
+	{
+		heights := []int{2, 4}
+		assert.Equal(t, 4, LargestRectangleArea(heights))
+	}
+	{
+		heights := []int{2, 1, 5, 6, 2, 3}
+		assert.Equal(t, 10, LargestRectangleArea(heights))
+	}
 }
