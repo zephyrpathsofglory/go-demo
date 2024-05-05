8c8a313b0b5a917cdbf3c2dc4ae2ddcf58b626f3
zephyrpathsofglory <zhangyonguu@gmail.com>
2024-05-05 17:22:20 +0800

    move exams

--- leetcode/84_test.go
+++ leetcode/84_test.go
@@ -1,13 +1,12 @@
-package largestrect_test
+package leetcode
 
 import (
 	"testing"
 
-	"github.com/go-demo/largestrect"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLargestRectangleArea(t *testing.T) {
 	heights := []int{1, 3, 4, 5, 2, 7, 4}
-	assert.Equal(t, 12, largestrect.LargestRectangleArea(heights))
+	assert.Equal(t, 12, LargestRectangleArea(heights))
 }
