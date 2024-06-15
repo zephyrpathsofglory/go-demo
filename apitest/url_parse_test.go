d121d84f55c7dd176345956d3304f2184efaf462
harden <harden@shundaojia.com>
2024-06-15 16:19:18 +0800

    trivial

--- apitest/url_parse_test.go
+++ apitest/url_parse_test.go
@@ -7,8 +7,22 @@ import (
 )
 
 func TestURLParse(t *testing.T) {
-	h, p, err := GetHostAndPortFromURL("schme://user:passwd@127.0.0.1:1777?sslmode=false")
-	assert.Nil(t, err)
-	assert.Equal(t, h, "127.0.0.1")
-	assert.Equal(t, p, "1777")
+	{
+		h, p, err := GetHostAndPortFromURL("schme://user:passwd@127.0.0.1:1777?sslmode=false")
+		assert.Nil(t, err)
+		assert.Equal(t, "127.0.0.1", h)
+		assert.Equal(t, "1777", p)
+	}
+	// {
+	// 	h, p, err := GetHostAndPortFromURL("schme://user/passwd@127.0.0.1:1777?sslmode=false")
+	// 	assert.Nil(t, err)
+	// 	assert.Equal(t, "127.0.0.1", h)
+	// 	assert.Equal(t, "1777", p)
+	// }
+	// {
+	// 	h, p, err := GetHostAndPortFromURL("user/passwd@127.0.0.1:1777?sslmode=false")
+	// 	assert.Nil(t, err)
+	// 	assert.Equal(t, "127.0.0.1", h)
+	// 	assert.Equal(t, "1777", p)
+	// }
 }
